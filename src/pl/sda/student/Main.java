4ac7f3659214f3336377169c6cc7d9ea85a1eaa8
kubaZ <zadrozinskijakub@gmail.com>
2020-02-29 11:48:12 +0100

    zapisywanie obiektu do json

--- src/pl/sda/student/Main.java
+++ src/pl/sda/student/Main.java
@@ -9,12 +9,22 @@ public class Main {
 
     public static void main(String[] args) {
         Student student = new Student("Jan", "Kowalski", 32);
+        Student student1 = new Student("Paweł", "Jankowski", 21);
+        Student student2 = new Student("Jacek", "Markiewicz", 23);
+        Student student3 = new Student("Jakub", "Jurkiewicz", 43);
+        Student student4 = new Student("Szymon", "Wiśniewski", 45);
+        Student student5 = new Student("Piotr", "Nowak", 25);
         // serializacja - np. obiektu do JSON-a
 
         ObjectMapper objectMapper = new ObjectMapper();
 
         try {
             objectMapper.writeValue(new File("student.json"),student);
+            objectMapper.writeValue(new File("student1.json"), student);
+            objectMapper.writeValue(new File("student2.json"), student);
+            objectMapper.writeValue(new File("student3.json"), student);
+            objectMapper.writeValue(new File("student4.json"), student);
+            objectMapper.writeValue(new File("student5.json"), student);
         } catch (IOException e) {
             e.printStackTrace();
         }

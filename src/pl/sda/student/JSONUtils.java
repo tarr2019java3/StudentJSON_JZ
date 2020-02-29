f8bfd984cf842a09c633f60d50e65836abcdd48e
kubaZ <zadrozinskijakub@gmail.com>
2020-02-29 13:48:44 +0100

    zapisywanie obiektu do json

--- src/pl/sda/student/JSONUtils.java
+++ src/pl/sda/student/JSONUtils.java
@@ -1,5 +1,38 @@
 package pl.sda.student;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.io.File;
+import java.io.IOException;
+import java.sql.SQLOutput;
+import java.util.List;
+
 public class JSONUtils {
-    
+
+    public void writeList(String filename, List<Student> students){
+        ObjectMapper objectMapper = new ObjectMapper();
+        try {
+            objectMapper.writeValue(new File(filename), students);
+
+    } catch (IOException e) {
+        e.printStackTrace();
+    }
+}
+
+    public void readList(String filename){
+
+        ObjectMapper objectMapper = new ObjectMapper();
+        try {
+        Student[] students = objectMapper.readValue(new File(filename),
+                Student[].class);
+        for(Student stu: students) {
+            System.out.println(stu.getName());
+        }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
 }
--- src/pl/sda/student/Main.java
+++ src/pl/sda/student/Main.java
@@ -10,32 +10,39 @@ import java.util.List;
 public class Main {
 
     public static void main(String[] args) {
-        Student student = new Student("Jan", "Kowalski", 32); //metoda1
-        Student student1 = new Student("Paweł", "Jankowski", 21); // metoda1
-        Student student2 = new Student("Jacek", "Markiewicz", 23); // metoda 1
-        Student student3 = new Student("Jakub", "Jurkiewicz", 43); // metoda 1
-        Student student4 = new Student("Szymon", "Wiśniewski", 45); // metoda 1
-        Student student5 = new Student("Piotr", "Nowak", 25);
+        Student student = new Student("Jan",
+                "Kowalski",
+                32);
+//        Student student1 = new Student("Paweł", "Jankowski", 21); // metoda1
+//        Student student2 = new Student("Jacek", "Markiewicz", 23); // metoda 1
+//        Student student3 = new Student("Jakub", "Jurkiewicz", 43); // metoda 1
+//        Student student4 = new Student("Szymon", "Wiśniewski", 45); // metoda 1
+//        Student student5 = new Student("Piotr", "Nowak", 25);
         // serializacja - np. obiektu do JSON-a
 
         ObjectMapper objectMapper = new ObjectMapper();
         List<Student> studentList = new ArrayList<>();
-        studentList.add(student1);
-       studentList.add(student2);
-       studentList.add(student3);
+        studentList.add(student);
+       studentList.add(student);
+       studentList.add(student);
 
 
-        try {
-       objectMapper.writeValue(new File("student_lista.json"), studentList);
-
-            objectMapper.writeValue(new File("student.json"),student);
-            objectMapper.writeValue(new File("student1.json"), student1); // metoda1
-            objectMapper.writeValue(new File("student2.json"), student2);
-            objectMapper.writeValue(new File("student3.json"), student3);
-            objectMapper.writeValue(new File("student4.json"), student4);
-            objectMapper.writeValue(new File("student5.json"), student5);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
+//        try {
+//       objectMapper.writeValue(new File("student_lista.json"), studentList);
+//
+//            objectMapper.writeValue(new File("student.json"),student);
+//            objectMapper.writeValue(new File("student1.json"), student); // metoda1
+//            objectMapper.writeValue(new File("student2.json"), student);
+//            objectMapper.writeValue(new File("student3.json"), student);
+//            objectMapper.writeValue(new File("student4.json"), student);
+//            objectMapper.writeValue(new File("student5.json"), student);
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
+//    }
+    JSONUtils jsonUtils = new JSONUtils();
+            jsonUtils.writeList("lista.json", studentList);
+            jsonUtils.readList("lista.json");
 }
+}
+
